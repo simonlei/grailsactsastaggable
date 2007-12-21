88da24f86aec33028fde9c78a146ed646b548bd7
simonlei <simonlei@c1e1030f-c241-0410-b2ea-6fcf8dc95b7e>
2007-12-21 03:46:00 +0000

    Version 0.1

--- trunk/testapp/test/integration/BookTests.groovy
+++ trunk/testapp/test/integration/BookTests.groovy
@@ -1,6 +1,64 @@
 class BookTests extends GroovyTestCase {
 
-    void testSomething() {
+    void testAddOneTag() {
+			Book book = new Book();
+			book.save()
 
+			book.addTag( "java")
+			assertEquals( "java", book.tags)
     }
+
+		void testAddMoreTags() {
+			Book book = new Book();
+			book.save()
+			
+			book.addTag( "java")
+			book.addTag( "groovy")
+			book.addTag( "grails")
+
+			assertEquals( "java groovy grails", book.tags)
+		}
+
+		void testAddTwoSameTags() {
+			Book book = new Book();
+			book.save()
+			
+			book.addTag( "java")
+			book.addTag( "java")
+
+			assertEquals( "java", book.tags)
+		}
+
+		void testRemoveNotExistTag() {
+			Book book = new Book();
+			book.save()
+			
+			book.addTag( "java")
+			book.removeTag( "notexist")
+			assertEquals( "java", book.tags)
+		}
+
+		void testRemoveOneTag() {
+			Book book = new Book();
+			book.save()
+			
+			book.addTag( "java")
+			book.addTag( "groovy")
+			book.addTag( "grails")
+
+			assertEquals( "java groovy grails", book.tags)
+
+			book.removeTag( "groovy")
+			assertEquals( "java grails", book.tags)
+		}
+	
+		void testSetTags() {
+			Book book = new Book();
+			book.save()
+
+			book.tags = "java groovy grails"
+			
+			book.removeTag( "groovy")
+			assertEquals( "java grails", book.tags)
+		}
 }
